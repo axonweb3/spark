4440e9420f4115a35c17a63a68df1b0b378c989d
michael <michael@michael>
2023-04-28 09:38:53 +0800

    style: fine-tune

--- query/src/adapter.rs
+++ query/src/adapter.rs
@@ -1,59 +1,44 @@
-
 use crate::QueryError;
-use common::{traits::query::QueryInformation, types::{RpcResult, query::NodeStakeHistory}};
 use async_trait::async_trait;
 use common::types::H256;
-pub struct DefaultQueryAdapter {
-
-}
+use common::{
+    traits::query::QueryInformation,
+    types::{query::NodeStakeHistory, RpcResult},
+};
+pub struct DefaultQueryAdapter {}
 
 #[async_trait]
 impl QueryInformation for DefaultQueryAdapter {
-    async fn get_stake_history(
-        &self,
-        address: H256,
-    ) -> RpcResult<NodeStakeHistory> {
+    async fn get_stake_history(&self, address: H256) -> RpcResult<NodeStakeHistory> {
         let res = NodeStakeHistory::default();
         Ok(res)
     }
 
-    async fn get_delegate_information(&self, address: H256) -> Vec<(H256)>{
+    async fn get_delegate_information(&self, address: H256) -> Vec<(H256)> {
         vec![H256::default()]
     }
 
-    async fn get_reward_information<'a>(
-        &self,
-        address: H256,
-    ) -> Vec<(H256)>{
+    async fn get_reward_information<'a>(&self, address: H256) -> Vec<(H256)> {
         vec![H256::default()]
     }
 
-    async fn get_reward_history<'a>(
-        &self,
-        address: H256,
-    ) -> Vec<(H256)>{
+    async fn get_reward_history<'a>(&self, address: H256) -> Vec<(H256)> {
         vec![H256::default()]
     }
 
-    async fn get_withdraw_history(
-        &self,
-        address: String,
-    ) -> Vec<(H256)>{
+    async fn get_withdraw_history(&self, address: String) -> Vec<(H256)> {
         vec![H256::default()]
     }
-        
-    async fn get_amount_info(&self) -> (H256, H256){
+
+    async fn get_amount_info(&self) -> (H256, H256) {
         (H256::default(), H256::default())
     }
 
-    async fn get_top_stake_info(&self) -> (H256, H256){
+    async fn get_top_stake_info(&self) -> (H256, H256) {
         (H256::default(), H256::default())
     }
 
-    async fn get_latest_stake_txs(
-        &self,
-        stake_type: H256,
-    ) -> Vec<(H256)>{
+    async fn get_latest_stake_txs(&self, stake_type: H256) -> Vec<(H256)> {
         vec![H256::default()]
     }
 }
@@ -61,9 +46,6 @@ impl QueryInformation for DefaultQueryAdapter {
 #[cfg(test)]
 mod tests {
 
-
     #[test]
-    fn test_tx_address() {
-        
-    }
+    fn test_tx_address() {}
 }
